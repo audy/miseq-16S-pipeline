d53566f9297d0597cb3993cd4cbc1ee93113ee4e
Austin Davis-Richardson <harekrishna@gmail.com>
2015-07-07 13:52:25 -0400

    added test/prepare.sh script

--- test/prepare.sh
+++ test/prepare.sh
@@ -1,1 +1,7 @@
-# todo: download greengenes & usearch61
+# todo: download greengenes
+
+curl ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz \
+  | gunzip | tar -xvf - \
+    gg_13_8_otus/taxonomy/97_otu_taxonomy.txt \
+    gg_13_8_otus/trees/97_otus.tree \
+    gg_13_8_otus/rep_set/97_otus.fasta
