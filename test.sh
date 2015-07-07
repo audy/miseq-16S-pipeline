5990dbbafcd5b72b68c962bd64e499a76c7ba7f9
Austin Davis-Richardson <harekrishna@gmail.com>
2015-07-07 10:39:25 -0400

    updated test

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -x
 set -e
 
@@ -5,6 +6,7 @@ left_reads='MS_ETriplett-103906_16S-BT0_2x300V3/Undetermined_S0_L001_R1_001.fast
 bc_reads='MS_ETriplett-103906_16S-BT0_2x300V3/Undetermined_S0_L001_I1_001.fastq'
 right_reads='MS_ETriplett-103906_16S-BT0_2x300V3/Undetermined_S0_L001_R2_001.fastq'
 
+# assemble overlapping, paired-end Illumina HiSeq reads
 pandaseq \
   -f $left_reads \
   -i $bc_reads \
@@ -12,9 +14,24 @@ pandaseq \
   -G log.txt.bz2 \
   > assembled.fasta
 
+# label assembled reads by barcode
 bin/label-by-barcode \
   --barcodes data/triplett-barcodes.csv \
   <  assembled.fasta \
   > labelled.fasta
 
-# usearch
+# make usearch database
+usearch \
+  -makeudb_usearch db/97_otus.fasta \
+  -output db/97_otus.udb
+
+# classify reads with usearch
+usearch \
+  -usearch_local labelled.fasta \
+  -id 0.97 \
+  -query_cov 0.95 \
+  -strand plus \
+  -uc labelled.uc \
+  -db db/97_otus.udb
+
+# generate OTU table
