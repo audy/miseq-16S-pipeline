e1d22db924a66b6b163707a5d61cc8e728c4cb74
Austin Davis-Richardson <harekrishna@gmail.com>
2015-07-28 13:04:06 -0400

    hpc opts

--- test/run.sh
+++ test/run.sh
@@ -8,6 +8,15 @@ right_reads='test/data/reads_R2.fastq'
 
 database='gg_13_8_otus/rep_set/97_otus.fasta'
 
+# determine number of threads. This is defined by PBS_NP if submitting a job.
+# Otherwise, default to 8.
+: ${PBS_NP:="8"}
+
+# check if running on UF HPC and load modules
+if [ ${PBS_O_QUEUE} ]; then
+  module load pandaseq/20150627
+  module load usearch/6.1.544-64
+fi
 
 # assemble overlapping, paired-end Illumina HiSeq reads
 pandaseq \
